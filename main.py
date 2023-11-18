d5cd15a29750e0f49e16441dec25c88564652495
Atsuko Uemura <atsukof.kt@gmail.com>
2023-11-17 23:00:53 -0800

    Add functions

--- main.py
+++ main.py
@@ -1,5 +1,125 @@
 
 def game():
+    """
+
+    :return:
+    """
+    pass
+
+def make_a_board():
+    """
+
+    :return:
+    """
+    pass
+
+def make_character():
+    """
+
+    :return:
+    """
+    pass
+
+def instruction():
+    """
+
+    :return:
+    """
+    pass
+
+def show_status_and_map():
+    """
+
+    :return:
+    """
+    pass
+
+def get_user_choice():
+    """
+
+    :return:
+    """
+    pass
+
+def validate_direction():
+    """
+
+    :return:
+    """
+    pass
+
+def move_user():
+    """
+
+    :return:
+    """
+    pass
+
+def is_kinkakuji():
+    """
+
+    :return:
+    """
+    pass
+
+def check_quiz():
+    """
+
+    :return:
+    """
+    pass
+
+def play_quiz():
+    """
+
+    :return:
+    """
+    pass
+
+def check_level_up():
+    """
+
+    :return:
+    """
+    pass
+
+def is_achieved_level_3():
+    """
+
+    :return:
+    """
+    pass
+
+def is_food_station():
+    """
+
+    :return:
+    """
+    pass
+
+def eat_food():
+    """
+
+    :return:
+    """
+    pass
+
+def fight_with_munk():
+    """
+
+    :return:
+    """
+    pass
+
+def lose_munk():
+    """
+
+    :return:
+    """
+    pass
+
+
+def game():
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, Chris')  # Press ⌘F8 to toggle the breakpoint.
 
