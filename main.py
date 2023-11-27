80433a5f501f52987851458d1637a6cb849abc2e
Misuzu Taniguchi <me.tpn23@gmail.com>
2023-11-27 11:12:29 -0800

    Create fight_with_monk function

--- main.py
+++ main.py
@@ -266,7 +266,23 @@ def fight_with_monk():
     """
 
     """
-    pass
+    while True:
+        # fight_dictionary = {1: 'rock', 2: 'paper', 3: 'scissors'}
+        monk_choice = random.randint(1, 3)
+        user_choice = int(input('Monk wants to fight with you. Which do you want to choose? '
+                                'Type 1 for rock, 2 for paper, 3 for scissors'))
+        if monk_choice == user_choice:
+            continue
+        elif monk_choice < user_choice:
+            if monk_choice == 1 and user_choice == 3:
+                return False
+            else:
+                return True
+        else:
+            if monk_choice == 3 and user_choice == 1:
+                return True
+            else:
+                return False
 
 
 def lose_monk():
