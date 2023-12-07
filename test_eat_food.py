c0e498d1a27fdcf76091252e6f52aeabd1807e12
Atsuko Uemura <atsukof.kt@gmail.com>
2023-12-07 00:45:42 -0800

    Update test_eat_food.py

--- test_eat_food.py
+++ test_eat_food.py
@@ -58,3 +58,21 @@ class Test(TestCase):
 
         self.assertEqual(expected_message, printed_message)
         self.assertEqual(4, my_character['Current HP'])
+
+    @patch('builtins.input', side_effect=['abc', 'n'])
+    @patch('sys.stdout', new_callable=io.StringIO)
+    def test_other_string_and_no(self, mock_output, _):
+        my_character = {
+            'X-coordinate': 3, 'Y-coordinate': 3, 'Current HP': 4, 'KEP': 0, 'Level': 'level 2', 'Name': 'Chris'
+        }
+        level_dict = {
+            "level 1": {"KEP_max": 2, "maximum_HP": 5, "name": "Kyoto rookie"},
+            "level 2": {"KEP_max": 5, "maximum_HP": 7, "name": "Kyoto skilled novice"},
+            "level 3": {"KEP_max": 9999, "maximum_HP": 10, "name": "Kyoto expert"},
+        }
+        eat_food(my_character, level_dict)
+        printed_message = mock_output.getvalue()
+        expected_message = 'You can only type "y" or "n". Try again.\nYou did\'t eat anything here.\n'
+
+        self.assertEqual(expected_message, printed_message)
+        self.assertEqual(4, my_character['Current HP'])
